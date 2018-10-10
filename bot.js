705f51c7a7188baeb7b201b0f2a5a7215ca6244d
rakanaltahhan <44034112+rakanaltahhan@users.noreply.github.com>
2018-10-10 21:30:34 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,89 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(` kill or sex ?  `,"http://twitch.tv/S-F")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online ')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
+client.on('message', message => {
+     if (message.content === "باي") {
+message.channel.sendMessage("" + " :rose:باي");
+    }
+}); 
+
+
+client.on('message', message => {
+     if (message.content === "spam?") {
+message.channel.sendMessage("يلا");
+    }
+}); 
+
+
+client.on('message', message => {
+     if (message.content === "برب") {
+message.channel.sendMessage("" + " :rose:تيت");
+    }
+}); 
+
+
+client.on('message', message => {
+     if (message.content === "هاي") {
+message.channel.sendMessage("" + "هايي");
+    }
+}); 
+
+client.on('message', message => {
+     if (message.content === "-help") {
+message.channel.sendMessage("" + "1 - نظام الرد التلقائـي العربي :robot:                                                                                                                                                                       4 - **(-bc <message>)** BCخاصية الـ                                                                                                      ");
+    }
+});
+
+
+client.on('message', message => {
+     if (message.content === "هلوو") {
+message.channel.sendEmbed("tt" + ":heart: هلوو");
+    }
+}); 
+
+
+client.on('message', message => {
+     if (message.content === "السلام " + "عليكم") {
+message.channel.sendMessage("" + "وعليكم السلام");
+    }
+});     
+
+
+client.on('message', message => {
+	 if (message.content === "wping") {
+	  const embed = new Discord.RichEmbed ()
+  
+  .setColor("#FF0000")
+  .addField('``سرعة إتصال الـبوت`` ' , `${Date.now() - message.createdTimestamp}` + ' ms`')
+
+  message.channel.sendEmbed(embed);
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
